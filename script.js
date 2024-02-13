a6532b4c89a262871aecb4ee98bf241a6e72aa61
whoever11 <cjthesedays000@gmail.com>
2024-02-13 10:19:46 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 console.log("Hello, World!")
 const randomNumber = Math.floor(Math.random() * 10);
 console.log("Random number:", randomNumber);
-ahhahhaeieiorruotupsuoffuapsututfppt
+// Path: script.js
+
+console.log("fun");
+
+function getRandomNumber() {
+   return Math.floor(Math.random() * 10)
+}
+
 
 // Path: index.html
